865b30db2f4e8eaf879ec161ba075b7fb0c60bc5
Gaurav Kanere <gaurav.kanere@gmail.com>
2023-03-08 13:49:02 +0530

    Added Cart and PurchaseHistory Models

---
+++ Models/DataModels/Cart.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Models.DataModels
+{
+    public class Cart
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int ProductId { get; set; }
+        [ForeignKey("Id")]
+        public string? UserId { get; set; }
+        public int Quantity { get; set; }
+        public Status Status { get; set; }
+    }
+    public enum Status { Added, Purchased }
+}
---
+++ Models/DataModels/PurchaseHistory.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Models.DataModels
+{
+    public class PurchaseHistory
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("ProductId")]
+        public int ProductId { get; set; }
+        [ForeignKey("Id")]
+        public int UserId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
