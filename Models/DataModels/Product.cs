4959ab3bd07783bbdad9356c137546ddb2a376c5
Gaurav Kanere <gaurav.kanere@gmail.com>
2023-03-08 12:44:14 +0530

    Add project files.

---
+++ Models/DataModels/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Shop.Models.DataModels
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+        public double Quantity { get; set;}
+        public bool Visiblity { get; set; }
+        public string UploadedBy { get; set; }
+    }
+}
