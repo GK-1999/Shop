a916bcdcdcf96c1c5f435a800ec9d5a05933e9fb
Gaurav Kanere <gaurav.kanere@gmail.com>
2023-03-08 20:06:14 +0530

    Login and Registration Model Created

---
+++ Models/ViewModels/RegistrationModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Models.ViewModels
+{
+    public class RegistrationModel
+    {
+       
+        [Required]
+        public string? UserName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string? EmailId { get; set; }
+        [Required]
+        public string? Password { get; set; }
+        [Required]
+        public string? FirstName { get; set; }
+        [Required]
+        public string? LastName { get; set; }
+        [Required]
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public int Pincode { get; set; }
+    }
+}
