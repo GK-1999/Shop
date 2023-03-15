f02a20df54c0bbcabbd99b0a645f9636e93db371
Gaurav Kanere <gaurav.kanere@gmail.com>
2023-03-15 19:25:31 +0530

    Created Product Services

---
+++ Services/Implementations/ProductServices.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Shop.DbContext;
+using Shop.Models.DataModels;
+using Shop.Models.Response;
+using Shop.Services.Interfaces;
+using System.Xml.Linq;
+
+namespace Shop.Services.Implementations
+{
+    public class ProductServices : IProductServices
+    {
+        ShopDbContext? _dbContext;
+
+        public ProductServices(ShopDbContext? dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public Task<response> AddProduct(Product model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<response> DeleteProductById(int id)
+        {
+            var product = _dbContext.Products.SingleOrDefault(x => x.ProductId == id);
+
+            //if (currentUser != product.UploadedBy)
+            //return new response { Message = $"No Such Product Found", StatusCode = 400 };
+
+            if (product == null) return new response { Message = $"No Product Found with of Id : {id}", StatusCode = 400 };
+            
+            _dbContext.Products.Remove(product);
+            _dbContext.SaveChanges();
+
+            return new response { Message = $"Product Sucessfully Deleted " , StatusCode = 200 };
+        }
+
+        public async Task<response> DeleteProductByName(string name)
+        {
+            var product = _dbContext.Products.SingleOrDefault(x => x.Name == name);
+
+            //if (currentUser != product.UploadedBy)
+            //return new response { Message = $"No Such Product Found", StatusCode = 400 };
+
+            if (product == null) return new response { Message = $"No Product Found with of Name : {name}", StatusCode = 400 };
+
+            _dbContext.Products.Remove(product);
+            _dbContext.SaveChanges();
+
+            return new response { Message = $"Product Sucessfully Deleted ", StatusCode = 200 };
+        }
+
+        public Task<response> UpdateProduct(Product model)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> ViewAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> ViewProductByAdmin(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> ViewProductByCategory(string category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> ViewProductById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> ViewProductByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/Interfaces/IProductServices.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Models.DataModels;
+using Shop.Models.Response;
+
+namespace Shop.Services.Interfaces
+{
+    public interface IProductServices
+    {
+        Task<response> AddProduct(Product model);
+        Task<response> UpdateProduct(Product model);
+        Task<response> DeleteProductById(int id);
+        Task<response> DeleteProductByName(string name);
+
+        Task<IActionResult> ViewAllProducts();
+        Task<IActionResult> ViewProductById(int Id);
+        Task<IActionResult> ViewProductByName(string name);
+        Task<IActionResult> ViewProductByAdmin(string name);
+        Task<IActionResult> ViewProductByCategory(string category);
+    }
+}
