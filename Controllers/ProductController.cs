3b13c4ed795066f72bb407e69ae649dff1ce9b9e
Gaurav Kanere <gaurav.kanere@gmail.com>
2023-03-23 13:37:18 +0530

    Created Product Controller

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Shop.Models.DataModels;
+using Shop.Models.ViewModels;
+using Shop.Services.Interfaces;
+
+namespace Shop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductServices _product;
+        IMapper _mapper;
+        public ProductController(IMapper mapper, IProductServices product)
+        {
+            _mapper = mapper;
+            _product = product;
+        }
+
+        [HttpPut("UpdateProduct")]
+        public IActionResult updateProduct([FromBody] UpdateProduct model) 
+        {
+            if(!ModelState.IsValid) return BadRequest(" Invalid Operation");
+
+            var product = _product.UpdateProduct(model);
+            if (product != null) return BadRequest("Product Not Updated");
+
+            return Ok(model);
+        }
+
+        [HttpPost("AddProduct")]
+        public IActionResult addProduct([FromBody] Product model)
+        {
+            if (!ModelState.IsValid) return BadRequest(" Invalid Operation");
+            
+            var product = _product.AddProduct(model);
+
+            if (product.IsCompleted) return Ok(product.Result.Message);
+
+            return BadRequest("Product Not Added");
+        }
+    }
+}
